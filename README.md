b48f52d37d2c6e261fd1012976049168567f9e36
ak532892 <ak532892@gmail.com>
2016-05-19 19:29:10 +0800

    comments

---
+++ README.md
@@ -0,0 +1,15 @@
+1.
+將bmp.c、image.c、image.h和
+任一資料夾中的.c檔(main)放在一起即可編譯
+
+2.
+產生出來的.exe檔 需用cmd下指令給參數
+或者用.bat檔來執行也可以
+
+第一個參數為exe檔
+第二個參數為輸入的圖片(bmp)
+第三個參數為輸出的圖片(bmp)
+輸入的圖片可以用in.bmp來測試
+
+ex: 
+test.exe in.bmp out.bmp
---
+++ highlow/highlow.c
@@ -0,0 +1,95 @@
+#include "..\image.h"
+#include <math.h>
+#include <stdlib.h>
+
+int swapval(int *v1,int *v2)
+{
+	int dum;
+
+	dum= *v1;
+	*v1 = *v2;
+	*v2 = dum;
+}
+
+main(int ac,char *av[])
+{
+	ImageData *img,*outimg;
+	int res;
+	int nx,ny;
+
+	if(ac<3) {
+		printf("ƒp??[ƒ^‚ª‘«‚è‚Ü‚¹‚ñ");
+		return;
+	}
+
+	res=readBMPfile(av[1],&img);
+	if(res<0) {
+		printf("‰æ?‚ª“Ç‚ß‚Ü‚¹‚ñ");
+		return;
+	}
+	nx=img->width;
+	ny=img->height;
+
+	outimg=createImage(nx,ny,24);
+
+	transform(img,outimg);
+
+	writeBMPfile(av[2],outimg);
+	disposeImage(img);
+	disposeImage(outimg);
+
+}
+
+
+
+int transform(ImageData *img,ImageData *outimg)
+{
+	int x,y;
+	int x1,y1;
+	int x2,y2;
+	int rx1,ry1,rx2,ry2;
+	Pixel col;
+	int h1,w1;
+	int h2,w2;
+	double fx,fy;
+	int i;
+
+	x1=0;
+	y1=0;
+	x2=outimg->width - 1;
+	y2=outimg->height - 1;
+
+	rx1=0;
+	ry1=0;
+	rx2=img->width - 1;
+	ry2=img->height - 1;
+
+	w1=rx2-rx1+1;
+	h1=ry2-ry1+1;
+	w2=x2-x1+1;
+	h2=y2-y1+1;
+	col.r=col.g=col.b=128;
+	for(y=y1,i=0;y<=y2;y++) {
+		for(x=x1;x<=x2;x++) {
+			fx=(double)(x-x1)*(w1)/(double)(w2)+rx1;
+			//¨Ì¤ñ¨Ò?Eú¨Ó·½¹Ïªº¹³¯?
+			fy=(double)(y-y1)*(h1+y-i)/(double)(h2+y2-i)+ry1;
+			interpolate(img,fx,fy,&col);
+			setPixel(outimg,x,y,&col);	
+		}
+		//¥Îi¨Ó±±¨û?Eú¤ñ¨?
+		if((y-y1)<(y2-y1)/2)
+			i+=2;
+	}
+}
+
+
+int interpolate(ImageData *img,double x,double y,Pixel *col)
+{
+	int px,py;
+
+
+	px=(int)(x+0.5);
+	py=(int)(y+0.5);
+	return getPixel(img,px,py,col);
+}
